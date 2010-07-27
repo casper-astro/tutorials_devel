ccd6db6a838160613c5220d49e742a1fe4588678
Mark Wagner <mwagner@ssl.berkeley.edu>
2010-07-27 23:03:22 +0000

    Minor, functionaly insignificant changes to tut3 spectrometer script and and model.  Added the most recent compile of the corresponding .bof file.

--- workshop_2010/roach_tut3_wideband_spec/spectrometer.py
+++ workshop_2010/roach_tut3_wideband_spec/spectrometer.py
@@ -12,7 +12,7 @@ You need to have KATCP and CORR installed. Get them from http://pypi.python.org/
 
 import corr,time,numpy,struct,sys,logging,pylab
 
-bitstream = 'r_spec_2048_r105_2010_May_04_1421.bof'
+bitstream = 'r_spec_2048_r105_2010_Jul_26_1205.bof'
 katcp_port=7147
 
 def exit_fail():
