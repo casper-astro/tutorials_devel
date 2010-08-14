9d2213868e638dcef9e99fc03b224ff19f0e2f1a
Mark Wagner <mwagner@ssl.berkeley.edu>
2010-08-14 15:26:44 +0000

    deleting superfluous model and bof files

--- workshop_2010/roach_tut3_wideband_spec/spectrometer.py
+++ workshop_2010/roach_tut3_wideband_spec/spectrometer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 '''
 This script demonstrates programming an FPGA, configuring a wideband spectrometer and plotting the received data using the Python KATCP library along with the katcp_wrapper distributed in the corr package. Designed for use with TUT3 at the 2009 CASPER workshop.\n
-
 You need to have KATCP and CORR installed. Get them from http://pypi.python.org/pypi/katcp and http://casper.berkeley.edu/svn/trunk/projects/packetized_correlator/corr-0.4.0/
 
 \nAuthor: Jason Manley, November 2009.
